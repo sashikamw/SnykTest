3f18d9d8ea35a82863f7841fef80279abe7311c0
sashikamw <sashimw@gmail.com>
2020-06-02 16:49:56 -0400

    initial

---
+++ config.js
@@ -0,0 +1,14 @@
+ //Configure all the screenshot variables here
+ module.exports = { 
+ BrowserSectionWithSmartScroll: {"windows 10": {"chrome": ["79"],"firefox": ["71"],"edge": ["17"]},"macos mojave": {"safari": ["12"]}},
+ BrowserSectionWithoutSmartScroll: {"windows 10": {"ie": ["11"]},"ios 12.0": {"devices": ["iphone xr"]},"ios 10.3": {"devices": ["iphone se"]},"android 9.0": {"devices": ["galaxy s9 plus"]}},
+ urllist: ["https://integration.hallerent.com/", "https://integration.hallerent.com/services/", "https://integration.hallerent.com/services/heating-cooling/", "https://integration.hallerent.com/careers/", "https://integration.hallerent.com/commercial/", "https://integration.hallerent.com/promotional-offers/", 
+    "https://integration.hallerent.com/payments-financing/", "https://integration.hallerent.com/blog/", "https://integration.hallerent.com/category/home-lifestyle/", "https://integration.hallerent.com/how-to-prepare-for-a-winter-storm/", "https://integration.hallerent.com/service-memberships/","https://integration.askgem.com/", 
+    "https://integration.askgem.com/services/", "https://integration.askgem.com/services/heating-cooling/", "https://integration.askgem.com/careers/", "https://integration.askgem.com/commercial/", "https://integration.askgem.com/promotional-offers/", 
+    "https://integration.askgem.com/payments-financing/", "https://integration.askgem.com/blog/", "https://integration.askgem.com/category/plumbing/", "https://integration.askgem.com/questions-about-bathroom-plumbing/", "https://integration.askgem.com/service-memberships/"],
+ defer_time: 3,
+ layout: "portrait",
+ mac_res: "1024x768",
+ win_res: "1366X768",
+ email: false
+};
